c3e6d00956a5620cc71d432a4ff6c9ad9d22c480
vaibhav <vaibhavginnalwar@gmail.com>
2024-10-15 11:33:46 +0530

    create simple todo page

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,16 @@ function App() {
   return (
     <>
       <div className="h-screen w-screen flex justify-center items-center">
-        Hello world!
+           <div className="w-[600px] p-3 rounded-md shadow-lg bg-[#242424]">
+                <h1 className="font-bold text-3xl">Todos</h1>
+                <p>Add your daily task</p>
+
+                <form action="">
+                    <div className="mt-5">
+                        <input type="text" className="w-full h-10 p-2 rounded bg-[#282828] outline-red-400 border border-red-400" placeholder="Enter your task" />
+                    </div>
+                </form>
+           </div>
       </div>
     </>
   )
