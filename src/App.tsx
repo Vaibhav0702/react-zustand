8a530136d6e27bf8dd610b4159b98f060f64c5e1
vaibhav <vaibhavginnalwar@gmail.com>
2024-10-15 11:11:01 +0530

    setUp todo with tailwind

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+
+  return (
+    <>
+      <div className="h-screen w-screen flex justify-center items-center">
+        Hello world!
+      </div>
+    </>
+  )
+}
+
+export default App
